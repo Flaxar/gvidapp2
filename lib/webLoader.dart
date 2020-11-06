afbbdd2bc14badc49dfc34b07a3d210bd62d0e96
Tomas Janousek <tomaszbrna@gmail.com>
2020-11-06 22:31:43 +0100

    Start on Webloader rework

--- lib/webLoader.dart
+++ lib/webLoader.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 // webLoader is a widget that is used for async and network functions
 // it downloads the data, shows them and handles errors
@@ -22,12 +24,16 @@ abstract class WebLoader<T> extends StatefulWidget {
 }
 
 class _WebLoaderState<S> extends State<WebLoader<S>> {
-  Widget build(BuildContext context) {
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+  S data;
+
+  Widget createFutureBuilder() {
     return FutureBuilder<S>(
       future: this.widget.calculation(),
       builder: (BuildContext context, AsyncSnapshot<S> snapshot) {
         if (snapshot.hasData) {
-          return this.widget.success(snapshot.data);
+          data = snapshot.data;
+          return this.widget.success(data);
         }
         else if (snapshot.hasError) {
           return this.widget.failure();
@@ -38,55 +44,110 @@ class _WebLoaderState<S> extends State<WebLoader<S>> {
       },
     );
   }
+
+  void _onRefresh() async{
+    /*try {
+      S someData = await this.widget.calculation();
+      print(someData);
+      setState(() {
+        data = someData;
+      });
+      _refreshController.refreshCompleted();
+    } catch (e) {
+      print(e);
+      _refreshController.refreshFailed();
+    }*/
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: false,
+        header: ClassicHeader(),
+        footer: CustomFooter(
+          builder: (BuildContext context,LoadStatus mode){
+            Widget body;
+            if(mode == LoadStatus.idle){
+              body = Text("pull up load");
+            }
+            else if(mode == LoadStatus.loading){
+              body = CupertinoActivityIndicator();
+            }
+            else if(mode == LoadStatus.failed){
+              body = Text("Load Failed! Click retry!");
+            }
+            else if(mode == LoadStatus.canLoading){
+              body = Text("release to load more");
+            }
+            else{
+              body = Text("No more Data");
+            }
+            return Container(
+              height: 55.0,
+              child: Center(child: body),
+            );
+          },
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        child: createFutureBuilder(),
+      ),
+    );
+  }
 }
 
 Widget createLoadingCircle(String text) {
   return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-          child: CircularProgressIndicator(),
-          width: 60,
-          height: 60,
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 16),
-          child: Text(text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text(text,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                  )
+              ),
             )
-          ),
-        )
-      ]
-    )
+          ]
+      )
   );
 }
 
 Widget createIconText(IconData icon, Color color, String text) {
   return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          color: color,
-          size: 60,
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 16),
-          child: Text(text,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20
-              )
-          ),
-        )
-      ]
-    )
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: color,
+              size: 60,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text(text,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                  )
+              ),
+            )
+          ]
+      )
   );
 }
 
